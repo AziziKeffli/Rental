a71e015c22f861176c97a2b17619674283489847
AziziKeffli <36685400+AziziKeffli@users.noreply.github.com>
2018-03-10 01:53:27 +0800

    Add project files.

---
+++ Rental/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
